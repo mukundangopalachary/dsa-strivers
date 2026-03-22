ae5c67eeab359554e365e980739f2213a5de25ad
mukundangopalachary <mukundangopalachary@gmail.com>
2026-03-22 07:23:47 +0530

    hashing

--- src/main/java/com/strivers/basics/hashing/CountFrequency.java
+++ src/main/java/com/strivers/basics/hashing/CountFrequency.java
@@ -16,6 +16,38 @@ public class CountFrequency {
     }
 
     System.out.println(hashmap);
+    
+    maxFrequency(arr);
+  }
 
+  public static void maxFrequency(int[] arr){
+    
+    Map<Integer, Integer> hashmap = new HashMap<>();
+
+    for(int n: arr){
+      hashmap.put(n, hashmap.getOrDefault(n, 0)+1);
+    }
+    
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+
+    int max_ele = 0, min_ele = 0;
+    for(Map.Entry<Integer, Integer> entry: hashmap.entrySet()){
+      int key = entry.getKey();
+      int value = entry.getValue();
+
+      if(value > max){
+        max = value;
+        max_ele = key;
+      }
+
+      if(value < min){
+        min = value;
+        min_ele = key;
+      }
+    }
+
+    System.out.println("Element with highest frequency: " + max_ele);
+    System.out.println("Element with lowest frequency: " + min_ele);
   }
 }
