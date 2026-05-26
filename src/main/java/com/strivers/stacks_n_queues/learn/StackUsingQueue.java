7c4dbe4b76fe11df80e05d05e3477729b9b5cd72
mukundangopalachary <mukundangopalachary@gmail.com>
2026-05-26 19:56:23 +0530

    valid parenthesis

--- src/main/java/com/strivers/stacks_n_queues/learn/StackUsingQueue.java
+++ src/main/java/com/strivers/stacks_n_queues/learn/StackUsingQueue.java
@@ -1,7 +1,28 @@
 package com.strivers.stacks_n_queues.learn;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StackUsingQueue {
+  Deque<Integer> stack;
 
-  private StackUsingArray<Integer> queue = new StackUsingArray<>();
+  public StackUsingQueue() {
+    stack = new ArrayDeque<>();
+  }
 
+  public void push(int x) {
+    stack.addFirst(x);
+  }
+
+  public int pop() {
+    return stack.removeFirst();
+  }
+
+  public int top() {
+    return stack.peekFirst();
+  }
+
+  public boolean empty() {
+    return stack.isEmpty();
+  }
 }
---
+++ src/main/java/com/strivers/stacks_n_queues/learn/ValidParenthesis.java
@@ -0,0 +1,30 @@
+package com.strivers.stacks_n_queues.learn;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char cur = s.charAt(i);
+      if (!stack.isEmpty()) {
+        char last = stack.peek();
+        if (isPair(last, cur)) {
+          stack.pop();
+          continue;
+        }
+      }
+      stack.push(cur);
+    }
+
+    return stack.isEmpty();
+  }
+
+  private boolean isPair(char last, char cur) {
+    return (last == '(' && cur == ')') ||
+        (last == '{' && cur == '}') ||
+        (last == '[' && cur == ']');
+  }
+}
