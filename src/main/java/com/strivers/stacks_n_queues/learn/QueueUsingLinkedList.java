1587fc6c52678df2bb6d76becfc40e77e21e53e3
mukundangopalachary <mukundangopalachary@gmail.com>
2026-05-27 21:23:14 +0530

    missed to remove duplicate Node class

--- src/main/java/com/strivers/stacks_n_queues/learn/QueueUsingLinkedList.java
+++ src/main/java/com/strivers/stacks_n_queues/learn/QueueUsingLinkedList.java
@@ -1,15 +1,5 @@
 package com.strivers.stacks_n_queues.learn;
 
-class Node {
-  int val;
-  Node next;
-
-  Node(int d) {
-    val = d;
-    next = null;
-  }
-}
-
 // Structure to represent queue
 class LinkedListQueue {
   private Node start; // Start of the queue
@@ -69,33 +59,5 @@ class LinkedListQueue {
   // Method to check if the queue is empty
   public boolean isEmpty() {
     return (size == 0);
-  }
-}
-
-class Main {
-  public static void main(String[] args) {
-    // Creating a queue
-    LinkedListQueue q = new LinkedListQueue();
-
-    // Array of commands
-    String[] commands = { "LinkedListQueue", "push", "push",
-        "peek", "pop", "isEmpty" };
-    // Array of inputs
-    int[][] inputs = { {}, { 3 }, { 7 }, {}, {}, {} };
-
-    for (int i = 0; i < commands.length; ++i) {
-      if (commands[i].equals("push")) {
-        q.push(inputs[i][0]);
-        System.out.print("null ");
-      } else if (commands[i].equals("pop")) {
-        System.out.print(q.pop() + " ");
-      } else if (commands[i].equals("peek")) {
-        System.out.print(q.peek() + " ");
-      } else if (commands[i].equals("isEmpty")) {
-        System.out.print((q.isEmpty() ? "true" : "false") + " ");
-      } else if (commands[i].equals("LinkedListQueue")) {
-        System.out.print("null ");
-      }
-    }
   }
 }
