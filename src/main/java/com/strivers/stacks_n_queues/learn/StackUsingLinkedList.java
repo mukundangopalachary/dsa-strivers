61d04c49431c77e69b391b881e7132c7a9b8c35b
mukundangopalachary <mukundangopalachary@gmail.com>
2026-05-27 14:31:04 +0530

    stack using linkedlist

---
+++ src/main/java/com/strivers/stacks_n_queues/learn/StackUsingLinkedList.java
@@ -0,0 +1,10 @@
+package com.strivers.stacks_n_queues.learn;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class StackUsingLinkedList {
+
+  private List<Integer> stack = new LinkedList<>();
+
+}
