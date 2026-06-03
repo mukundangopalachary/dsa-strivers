f478553dc80a65afa2c04494cd6b83ca38d0c360
mukundangopalachary <mukundangopalachary@gmail.com>
2026-06-03 07:38:35 +0530

    largest rectangle in a histogram

---
+++ src/main/java/com/strivers/stacks_n_queues/problems_i/LargestRectangleInAHistogram.java
@@ -0,0 +1,33 @@
+package com.strivers.stacks_n_queues.problems_i;
+
+import java.util.Stack;
+
+public class LargestRectangleInAHistogram {
+
+  public int largestRectangleArea(int[] heights) {
+    Stack<Integer> st = new Stack<>();
+    int maxArea = 0;
+    int n = heights.length;
+
+    for (int i = 0; i <= n; i++) {
+      int currHeight = (i == n) ? 0 : heights[i];
+
+      while (!st.isEmpty() && heights[st.peek()] > currHeight) {
+        int height = heights[st.pop()];
+
+        int width;
+        if (st.isEmpty()) {
+          width = i;
+        } else {
+          width = i - st.peek() - 1;
+        }
+
+        maxArea = Math.max(maxArea, height * width);
+      }
+
+      st.push(i);
+    }
+
+    return maxArea;
+  }
+}
