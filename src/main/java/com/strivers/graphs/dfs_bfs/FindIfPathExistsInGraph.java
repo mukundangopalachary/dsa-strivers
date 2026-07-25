6d720ff9e5f61bff93f0303b8aad2302ca14120b
mukundangopalachary <mukundangopalachary@gmail.com>
2026-07-25 11:20:21 +0530

    find if path from source to dest exists completed

--- src/main/java/com/strivers/graphs/dfs_bfs/FindIfPathExistsInGraph.java
+++ src/main/java/com/strivers/graphs/dfs_bfs/FindIfPathExistsInGraph.java
@@ -1,6 +1,9 @@
 package com.strivers.graphs.dfs_bfs;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
 
 public class FindIfPathExistsInGraph {
@@ -8,6 +11,39 @@ public class FindIfPathExistsInGraph {
     if (source == 0 && destination == 0) return true;
 
     List<List<Integer>> adj = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      adj.add(new ArrayList<>());
+    }
+
+    for (int[] edge : edges) {
+      int u = edge[0];
+      int v = edge[1];
+
+      adj.get(u).add(v);
+      adj.get(v).add(u);
+    }
+
+    boolean[] visited = new boolean[n];
+    Arrays.fill(visited, false);
+
+    Deque<Integer> q = new ArrayDeque<>();
+    q.offer(source);
+
+    visited[source] = true;
+
+    while (!q.isEmpty()) {
+      int current = q.poll();
+
+      if (destination == current) return true;
+
+      for (int num : adj.get(current)) {
+        if (!visited[num]) {
+          visited[num] = true;
+          q.offer(num);
+        }
+      }
+    }
 
     return false;
   }
