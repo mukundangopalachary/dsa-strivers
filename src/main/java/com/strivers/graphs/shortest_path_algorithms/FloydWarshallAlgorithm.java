0d3a92ce43ca9254172f0a4837106558bd8ae10a
Mukundan Gopalachary <mukundangopalachary@gmail.com>
2026-08-17 08:16:48 +0530

    find the city floyd warshall

--- src/main/java/com/strivers/graphs/shortest_path_algorithms/FloydWarshallAlgorithm.java
+++ src/main/java/com/strivers/graphs/shortest_path_algorithms/FloydWarshallAlgorithm.java
@@ -1,8 +1,30 @@
 package com.strivers.graphs.shortest_path_algorithms;
 
 public class FloydWarshallAlgorithm{
+    int INF = (int) 1e8;
 
-    public int[] floydWarshallAlgorithm(){
+    public int[][] floydWarshallAlgorithm(int[][] matrix){
+        
+        if(matrix.length == 0) return matrix;
 
+        int V = matrix.length;
+
+        // intermediate        
+        for(int k = 0; k < V; k++){
+
+            //row
+            for(int i = 0; i < V; i++){
+
+                //col
+                for(int j = 0; j < V; j++){
+
+                    if(matrix[i][k] != INF && matrix[k][j] != INF){
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }
+
+        return matrix;
     }
 }
